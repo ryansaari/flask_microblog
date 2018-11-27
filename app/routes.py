53c7563db0a334d58e2db197fa431ccd8ca83315
Ryan Saari <ryansaari@outlook.com>
2018-11-26 21:00:19 -0600

    Translation API
    When adding a server/client model translation for posts, the live translation is going to come from a  Microsoft Azure API. This commit contains all of that setup before altering the server or the client.

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
 from flask_babel import _, get_locale
+from guess_language import guess_language
 
 # Flask has a built in feature to send generic logic ahead of a request being
 # dispatched to a view functions. This updates last seen
@@ -48,7 +49,10 @@ def logout():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
