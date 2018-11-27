581b6840e5722a9afbd551eef7f8277dc145f8e0
Ryan Saari <ryansaari@outlook.com>
2018-11-26 22:33:04 -0600

    Translation Debugging
    Some errors made their way through when sloppily checking translations. There may be some things broken still, but it seems to pass my tests.

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 20
-    LANGUAGES = ['en', 'es']
+    LANGUAGES = ['en']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
