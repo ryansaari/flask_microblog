ee3d5c36084d3eb6c67e415e2326d6f53fbd033b
Ryan Saari <ryansaari@outlook.com>
2018-11-26 18:32:55 -0600

    Added flask-babel
    Added babel and marked all of the text that needs to be translated

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -20,10 +21,18 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 boostrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 # allow Flask-Login to know what is the view function that handles logins
 # 'login' here is the endpoint for the login view
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
+
+# this decorated function is invoked for each request to select a language
+# translation for use for that request
+@babel.localselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 # add an smtphandler instance to the flask logger object
 if not app.debug:
--- app/forms.py
+++ app/forms.py
@@ -2,39 +2,40 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
+from flask_babel import _, lazy_gettext as _l
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField(_('Username')), validators=[DataRequired()])
+    password = PasswordField(_('Password')), validators=[DataRequired()])
+    remember_me = BooleanField(_('Remember Me'))
+    submit = SubmitField(_('Sign In'))
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField(_('Username'), validators=[DataRequired()])
+    email = StringField(_('Email'), validators=[DataRequired(), Email()])
+    password = PasswordField(_('Password'), validators=[DataRequired()])
     password2 = PasswordField(
-        'Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+        _('Confirm Password'), validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField(_l('Register'))
 
     # validate_x where x = <field_name> are automatically applied to their field
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError('That username is already taken')
+            raise ValidationError(_('Please use a different username.'))
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError(
-                'There is already an account registered to that email')
+                _('Please use a different email address.'))
 
 # This form lets the useres change their username and write an about me
 class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
-    submit = SubmitField('Submit')
+    username = StringField(_l('Username'), validators=[DataRequired()])
+    about_me = TextAreaField(_l('About me'), validators=[Length(min=0, max=140)])
+    submit = SubmitField(_l('Submit'))
 
 # saves the username as an instance variable and used to check to see if the Username
 # is still the one the form started with
@@ -46,23 +47,23 @@ class EditProfileForm(FlaskForm):
         if username.data != self.original_username:
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
-                raise ValidationError('Please use a different username.')
+                raise ValidationError(_('Please use a different username.'))
 
 # for to write a post
 class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[
+    post = TextAreaField(_l('Say something'), validators=[
         DataRequired(), Length(min=1, max=140)])
-    submit = SubmitField('Submit')
+    submit = SubmitField(_l('Submit'))
 
 # a form that the user will fill out to generate a password resest link
 class ResetPasswordRequestForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Reset Password Request')
+    email = StringField(_l('Email'), validators=[DataRequired(), Email()])
+    submit = SubmitField(_l('Reset Password Request'))
 
  # a form that the user will user to generate a new password
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField(_l('Password'), validators=[DataRequired()])
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')]
+        _l('Repeat Password'), validators=[DataRequired(), EqualTo('password')]
     )
-    submit = SubmitField('Request Password Reset')
+    submit = SubmitField(_l('Request Password Reset'))
