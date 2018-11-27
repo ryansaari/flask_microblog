dd547abe89d1cc8c6c956387857cf4ada7a2f246
Ryan Saari <ryansaari@outlook.com>
2018-11-26 18:59:20 -0600

    Added locale for time

--- app/routes.py
+++ app/routes.py
@@ -17,6 +17,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
